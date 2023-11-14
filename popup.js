afb331c9886451d81dc0d3c5d1364350a8058c5a
Brock <8839595+brocksprogramming@users.noreply.github.com>
2023-11-13 23:00:12 -0800

    GPL license added

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
+/*
+Copyright Brock Lynch 2020-2023
+
+This file is part of Chrome Bookmarked Website Dowloader.
+
+Chrome Bookmarked Website Dowloader is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Chrome Bookmarked Website Dowloader is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Chrome Bookmarked Website Dowloader. If not, see <https://www.gnu.org/licenses/>.
+*/
 window.addEventListener('DOMContentLoaded', (event) => {
 // Traverse the bookmark tree, and print the folder and nodes.
 function addBookmarks() {
